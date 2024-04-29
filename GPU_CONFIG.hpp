78c2dd679a7b8a86da1cace3feefd2c78bbd5c73
sachd-vipu <sachd.vipu@gmail.com>
2024-04-28 23:05:34 -0400

    Add SimulationConfig File

---
+++ GPU_CONFIG.hpp
@@ -0,0 +1,15 @@
+#ifndef GPU_CONFIG
+#define GPU_CONFIG
+
+extern int COMPUTE_UNITS = 20;
+constexpr int WARP_SIZE = 32;
+constexpr float PI = 3.1415926;
+constexpr float DAMP = 1.0;
+constexpr float GRAVITY = 1.0;
+constexpr int ITERATIONS = 20;
+constexpr float TIMESTEP = 0.001;
+constexpr bool PLOT_OPENGL = true;
+constexpr int GRID_SIZE = 256;
+constexpr int BLOCK_SIZE = 128;
+#endif 
+
--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 #include <string.h>
 #include <bits/stdc++.h>
 #include "NBodiesSimulation.hpp"
+#include "GPU_CONFIG.hpp"
 
 using namespace std;
 
 
-void displayDeviceProperties()
+void displayGPUProp_setSM()
 {
 	// Set up CUDA device 
 	cudaDeviceProp properties;
@@ -19,26 +20,30 @@ void displayDeviceProperties()
 	cudaDriverGetVersion(&driverVersion);
     cudaRuntimeGetVersion(&runtimeVersion);
 
+
 	std::cout << "************************************************************************" << std::endl;
 	std::cout << "                          NVIDIA CUDA device Properties                 " << std::endl;
 	std::cout << "************************************************************************" << std::endl;
 	std::cout << "Name:                                    " << properties.name << std::endl;
-	std::cout << "CUDA driver/runtime version:             " << driverVersion/1000 << "." << (driverVersion%100)/10 << "/" << runtimeVersion/1000 << "." << (runtimeVersion%100)/10 << std::endl;
-	std::cout << "CUDA compute capabilitiy:                " << properties.major << "." << properties.minor << std::endl;
-	std::cout << "Number of multiprocessors:               " << properties.multiProcessorCount << std::endl;                           
-	std::cout << "GPU clock rate:                          " << properties.clockRate/multiplier << " (MHz)" << std::endl;
-	std::cout << "Memory clock rate:                       " << properties.memoryClockRate/multiplier << " (MHz)" << std::endl;
-	std::cout << "Memory bus width:                        " << properties.memoryBusWidth << "-bit" << std::endl;
-	std::cout << "Theoretical memory bandwidth:            " << (properties.memoryClockRate/multiplier*(properties.memoryBusWidth/8)*2)/multiplier <<" (GB/s)" << std::endl;
+	//std::cout << "CUDA driver/runtime version:             " << driverVersion/1000 << "." << (driverVersion%100)/10 << "/" << runtimeVersion/1000 << "." << (runtimeVersion%100)/10 << std::endl;
+	//std::cout << "CUDA compute capabilitiy:                " << properties.major << "." << properties.minor << std::endl;
+	std::cout << "Number of Compute Units (SM's):          " << properties.multiProcessorCount << std::endl;                           
+	std::cout << "GPU clock :                          " << properties.clockRate/multiplier << " (MHz)" << std::endl;
+	std::cout << "Memory clock :                       " << properties.memoryClockRate/multiplier << " (MHz)" << std::endl;
+//	std::cout << "Memory bus width:                        " << properties.memoryBusWidth << "-bit" << std::endl;
+//	std::cout << "Theoretical memory bandwidth:            " << (properties.memoryClockRate/multiplier*(properties.memoryBusWidth/8)*2)/multiplier <<" (GB/s)" << std::endl;
 	std::cout << "Device global memory:                    " << properties.totalGlobalMem/(multiplier*multiplier) << " (MB)" << std::endl;
 	std::cout << "Shared memory per block:                 " << properties.sharedMemPerBlock/multiplier <<" (KB)" << std::endl;
 	std::cout << "Constant memory:                         " << properties.totalConstMem/multiplier << " (KB)" << std::endl;
-	std::cout << "Maximum number of threads per block:     " << properties.maxThreadsPerBlock << std::endl;
+//	std::cout << "Maximum number of threads per block:     " << properties.maxThreadsPerBlock << std::endl;
 	std::cout << "Maximum thread dimension:                [" << properties.maxThreadsDim[0] << ", " << properties.maxThreadsDim[1] << ", " << properties.maxThreadsDim[2] << "]" << std::endl;
 	std::cout << "Maximum grid size:                       [" << properties.maxGridSize[0] << ", " << properties.maxGridSize[1] << ", " << properties.maxGridSize[2] << "]" << std::endl;
+	std::cout << "Warp size:                               " << properties.warpSize << std::endl;
 	std::cout << "**************************************************************************" << std::endl;
 	std::cout << "                                                                          " << std::endl;
 	std::cout << "**************************************************************************" << std::endl;
+
+	COMPUTE_UNITS = properties.multiProcessorCount;
 }
 
 
@@ -46,9 +51,8 @@ void displayDeviceProperties()
 
 int main(int argc, char** argv)
 {
-	int num_bodies = 512*256;  
-	displayDeviceProperties();
-
+	int num_bodies = GRID_SIZE * BLOCK_SIZE;  
+	displayGPUProp_setSM();
 	NBodiesSimulation simulation(num_bodies);
 	simulation.runAnimation();
 	//simulation.~NBodiesSimulation();
